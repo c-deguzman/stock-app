962450289545d74c22f7b10b9d20a6bfaf4b8397
commando5671 <commando5671@users.noreply.github.com>
2017-08-09 01:39:56 -0400

    Implemented graph display

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import $ from 'jquery';
 import Websocket from 'react-websocket';
-import d3 from 'd3';
-import { LineChart } from 'react-d3-basic'
+//import d3 from 'd3';
+import { Chart } from 'react-d3-core';
+import { LineChart } from 'react-d3-basic';
+import { LineTooltip, SimpleTooltip } from 'react-d3-tooltip';
 
 
 export default class HomePage extends React.Component {
@@ -22,6 +24,8 @@ export default class HomePage extends React.Component {
     this.collect_chart_data = this.collect_chart_data.bind(this);
     this.finished_async_calls = this.finished_async_calls.bind(this);
 
+    this.xAccessor = this.xAccessor.bind(this);
+
     this.state = {
       time: "",
       ws_url: "",
@@ -29,8 +33,17 @@ export default class HomePage extends React.Component {
       current_list: [],
       current_data: [],
       historical: [],
+      chart_data: [],
+      chart_series: [],
       require_update: true
     }
+  }
+
+  xAccessor(datum){
+    //var parseTime = d3.timeParse("%Y-%m-%d");
+    var parseTime = d3.time.format("%Y-%m-%d").parse;
+
+    return parseTime(datum.time);
   }
 
   select_ticker(datum){
@@ -56,12 +69,12 @@ export default class HomePage extends React.Component {
     return "" + date.getFullYear() + "-" + this.pad_two(date.getMonth() + 1) + "-" + this.pad_two(date.getDate());
   }
 
-  collect_chart_data(){
+  collect_chart_data(days){
     var curr_time = new Date();
     var chart_data = [];
 
-    for (var i = 0; i <= 365; i++){
-      var date_str = this.add_date(curr_time.getFullYear() - 1, curr_time.getMonth(), curr_time.getDate(), i);
+    for (var i = 0; i <= days; i++){
+      var date_str = this.add_date(curr_time.getFullYear(), curr_time.getMonth(), curr_time.getDate(), i * -1);
 
       var local_obj = {time: date_str};
 
@@ -84,7 +97,21 @@ export default class HomePage extends React.Component {
       }
     }
 
-    return chart_data;
+    var chart_series = [];
+
+    for (var k in this.state.current_list){
+      var local_obj_ii = {};
+
+      local_obj_ii["field"] = this.state.current_list[k];
+      local_obj_ii["name"] = this.state.current_list[k];
+
+      chart_series.push(local_obj_ii);
+    }
+
+    this.setState({
+      chart_data: chart_data,
+      chart_series: chart_series
+    })
   }
 
   finished_async_calls(stock_data){
@@ -99,9 +126,8 @@ export default class HomePage extends React.Component {
       }
     }
 
-    this.setState({
-      chart_data: this.collect_chart_data()
-    }, () => console.log("Finished all async calls. State has been updated."));
+    this.collect_chart_data(365);
+    console.log("Finished all async calls. State has been updated.");
   }
 
 
@@ -176,9 +202,44 @@ export default class HomePage extends React.Component {
         <button className="btn btn-default" 
                 onClick={() => console.log(this.state)}> State Log </button>
         <p> {this.state.time} </p>
+
+        <div className="btn-group">
+            <button className="btn btn-default" onClick={() => this.collect_chart_data(7)}>1 week</button>
+            <button className="btn btn-default" onClick={() => this.collect_chart_data(31)}>1 Month</button>
+            <button className="btn btn-default" onClick={() => this.collect_chart_data(180)}>6 Months</button>
+            <button className="btn btn-default" onClick={() => this.collect_chart_data(365)}>1 Year</button>
+            <button className="btn btn-default" onClick={() => this.collect_chart_data(365 * 5)}>5 Years</button>
+          </div>
+
+
         { 
           this.state.display_imports ?
-          <Websocket url={this.state.ws_url} onMessage={this.handleData} /> :
+          <div>
+          <Websocket url={this.state.ws_url} onMessage={this.handleData} /> 
+
+          
+          <div className="centre">
+            <div id="chart_wrapper">
+            <LineTooltip
+              title="Stock History"
+              data={this.state.chart_data}
+              width={1000}
+              height={500}
+              margins={{left: 100, right: 50, top: 50, bottom: 50}}
+              chartSeries={this.state.chart_series}
+              x={this.xAccessor}
+              xScale="time" 
+              xLabel="Date"
+              yLabel="Stock Price (USD)"
+
+            >
+              <SimpleTooltip />
+            </LineTooltip>
+            </div>
+          </div>
+          
+
+          </div>:
           null
         }
         
